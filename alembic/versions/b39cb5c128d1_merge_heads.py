02675ff8c9c49d0702d809cc9dc0fc2c886fe3e7
Wojciech Przybytek <wojciech.przybytek@placewise.com>
2024-12-15 01:55:22 +0100

    Merge heads

---
+++ alembic/versions/b39cb5c128d1_merge_heads.py
@@ -0,0 +1,26 @@
+"""Merge heads
+
+Revision ID: b39cb5c128d1
+Revises: 71670c745310, c494ed642329
+Create Date: 2024-12-15 01:54:46.523935
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b39cb5c128d1'
+down_revision: Union[str, None] = ('71670c745310', 'c494ed642329')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
